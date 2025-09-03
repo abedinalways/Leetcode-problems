1d64f558c20d67fa807a52067bfc79b3e46e40be
abedinalways <nion.sk5067@gmail.com>
2025-09-03 13:14:38 +0600

    one problem solve

--- index.js
+++ index.js
@@ -63,3 +63,27 @@ let romanTointeger = function (s) {
     return total;
 }
 console.log(romanTointeger('MCMXCIV'));
+
+//Longest Common Prefix
+
+function longestCommonPrefix(strs) {
+  if (!strs.length) return "";
+
+  for (let i = 0; i < strs[0].length; i++) {
+    const char = strs[0][i];
+
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== char) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+
+  return strs[0];
+}
+
+// Example usage
+const words = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(words)); // Output: "fl"
+
+
